8499247bc384658359ed5a2a2d112606083101be
Alexei Yasko <alexei.yasko@gmail.com>
2011-12-22 23:41:51 +0300

    Add category

---
+++ src/cookbook/model/Category.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cookbook.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Category {
+    String name = "";
+
+    public Category() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
