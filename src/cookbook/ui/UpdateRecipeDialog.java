b27bc70a8b21751d9e0268790d3b729121002f73
Anton Sidorovich <cidyuk@gmail.com>
2011-12-23 15:33:29 +0200

    Combox viewing

--- src/cookbook/ui/UpdateRecipeDialog.java
+++ src/cookbook/ui/UpdateRecipeDialog.java
@@ -4,9 +4,16 @@
  */
 package cookbook.ui;
 
+import cookbook.model.Advice;
+import cookbook.model.Category;
+import cookbook.model.CookBookManager;
+import cookbook.model.Ingredient;
+import cookbook.model.Recipe;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -30,9 +37,9 @@ public class UpdateRecipeDialog extends JDialog {
     private JButton okButton;
     private JButton cancelButton;
 
-    public UpdateRecipeDialog(JFrame owner) {
+    public UpdateRecipeDialog(JFrame owner, Recipe recipe) {
         super(owner, "New recipe", true);
-        this.setSize(300, 300);
+        this.setSize(300, 600);
         this.setLocation(125, 125);
 
         name = new JTextField();
@@ -40,11 +47,18 @@ public class UpdateRecipeDialog extends JDialog {
         ingr = new JTextArea(10, 20);
         adv = new JTextArea(10, 20);
 
+        CookBookManager manager = new CookBookManager();
+
         JScrollPane descrScroll = new JScrollPane(descr);
         JScrollPane ingrScroll = new JScrollPane(ingr);
         JScrollPane advScroll = new JScrollPane(adv);
 
-        category = new JComboBox(new String[]{"Salads", "Soups", "Meat", "Garnishes"});
+        List<String> cat = new ArrayList<String>();        
+        for (Category x : manager.getAllCategory()) {
+            cat.add(x.getName());
+        }
+        
+        category = new JComboBox(cat.toArray());
 
         okButton = new JButton("Ok");
         cancelButton = new JButton("Cancel");
@@ -59,24 +73,40 @@ public class UpdateRecipeDialog extends JDialog {
         JPanel panLabel1 = new JPanel();
         panLabel1.add(new JLabel("Recipe name:"));
         vBox.add(panLabel1);
+        name.setText(recipe.getName());
         vBox.add(name);
         vBox.add(Box.createVerticalStrut(10));
 
         JPanel panLabel2 = new JPanel();
         panLabel2.add(new JLabel("Description: "));
         vBox.add(panLabel2);
+        descr.setText(recipe.getDescription());
         vBox.add(descrScroll);
         vBox.add(Box.createVerticalStrut(10));
 
         JPanel panLabel3 = new JPanel();
         panLabel3.add(new JLabel("Ingredients: (FORMAT: name quantity dimension;)"));
         vBox.add(panLabel3);
+
+        StringBuilder str = new StringBuilder();
+        for (Ingredient x : recipe.getIngridients()) {
+            str.append(x.getName() + " " + x.getQuantity() + "" + x.getDimension() + "\n");
+        }
+        ingr.setText(str.toString());
+
         vBox.add(ingrScroll);
         vBox.add(Box.createVerticalStrut(10));
 
         JPanel panLabel4 = new JPanel();
-        panLabel4.add(new JLabel("Description: (FORMAT: advice;)"));
+        panLabel4.add(new JLabel("Advice: (FORMAT: advice;)"));
         vBox.add(panLabel4);
+
+        str = new StringBuilder();
+        for (Advice x : recipe.getAdvices()) {
+            str.append(x.getDescription() + "\n");
+        }
+        adv.setText(str.toString());
+
         vBox.add(advScroll);
         vBox.add(Box.createVerticalStrut(10));
 
@@ -104,11 +134,11 @@ public class UpdateRecipeDialog extends JDialog {
     }
 
     public String[] getIngr() {
-        return ingr.getText().split("; ");
+        return ingr.getText().split("\n ");
     }
 
     public String[] getAdv() {
-        return adv.getText().split("; ");
+        return adv.getText().split("\n ");
     }
 
     private class ButtonHandler implements ActionListener {
